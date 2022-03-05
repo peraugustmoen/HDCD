3df81f2c780f2522a0bd8f1573c667dd467fd2a2
peraugustmoen <peraugust.pa@gmail.com>
2022-03-06 00:31:02 +0100

    some more simulations

--- inst/HDCDtestrun.R
+++ inst/HDCDtestrun.R
@@ -3,12 +3,16 @@ library(HDCD)
 
 #multiple change-points:
 p = 1000
-n =100
+n =1000
 mus = matrix(0, nrow=p, ncol=n)
 noise = matrix(rnorm(n*p), nrow=p, ncol=n)
-etas = c(round(n/3),round(2*n/3))
-mus[,round(etas[1]+1):n] = mus[,round(etas[1]+1):n] + matrix(rep(c(rnorm(10), rep(0,p-10)), n-round(etas[1])) ,nrow=p)
-mus[,round(etas[2]+1):n] = mus[,round(etas[2]+1):n] + matrix(rep(rnorm(p)/5, n-round(etas[2])) ,nrow=p)
+etas = c(round(n/4),round(2*n/4),round(3*n/4))
+randomdense =rnorm(p)
+randomdense = randomdense/norm(randomdense,type="2")*sqrt(p)
+randomdense = randomdense/p^(1/4)/sqrt(n)*12
+mus[,round(etas[1]+1):n] = mus[,round(etas[1]+1):n] + matrix(rep(c(rnorm(10)*2/sqrt(n)*10, rep(0,p-10)), n-round(etas[1])) ,nrow=p)
+mus[,round(etas[2]+1):n] = mus[,round(etas[2]+1):n] + matrix(rep(randomdense, n-round(etas[2])) ,nrow=p)
+mus[,round(etas[3]+1):n] = mus[,round(etas[3]+1):n] + matrix(rep(rep(1,p)/p^(1/4)/sqrt(n)*12, n-round(etas[3])) ,nrow=p)
 plot(mus[1,])
 X = mus+noise
 
@@ -24,7 +28,8 @@ res = Inspect(X, xi=xi, alpha = 1.2, K = 10,eps=1e-10,
               lambda = lambda, maxiter=10000,debug=FALSE)
 res$changepoints
 
-res2 = HDCD (X, 8, 2, alpha = 1+1/6, K = 7, debug =FALSE)
+res2 = HDCD (X, 4, 1, alpha = 1+1/6, K = 7, debug =FALSE)
+#NOTE: 8, 2 corresponds to EQUAL PENALTIES
 res2$changepoints
 res2$coordinate
 res2$CUSUMval
