c6573131780d7914e92ceb801c15a28a8f79190c
Per August Moen <peraugust.pa@gmail.com>
2023-05-08 11:35:32 +0200

    committtt

--- inst/hdcd_calibrate_LOAD.R
+++
@@ -1,9 +0,0 @@
-dir = "/Users/peraugust/Library/CloudStorage/OneDrive-UniversitetetiOslo/project1/simulations/Simulations_HDCD/2022-11-10--20.53.57/single"
-
-const1 = readRDS(paste(dir, "/const1.RDA", sep=""))
-const2 = readRDS(paste(dir, "/const2.RDA", sep=""))
-mse_avg1 = readRDS(paste(dir, "/mse_avg1.RDA", sep=""))
-mse_avg2 = readRDS(paste(dir, "/mse_avg2.RDA", sep=""))
-mse_avg3 = readRDS(paste(dir, "/mse_avg3.RDA", sep=""))
-mse_avg4 = readRDS(paste(dir, "/mse_avg4.RDA", sep=""))
-
